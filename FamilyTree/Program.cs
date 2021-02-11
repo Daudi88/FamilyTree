fab9ecaa9c5ea3d6b2b957c488476f110e8e0104
David Ström <david.strom88@gmail.com>
2021-02-11 20:18:28 +0100

    Method for creating database and tables done! Method for executing Sql done.

--- FamilyTree/Program.cs
+++ FamilyTree/Program.cs
@@ -6,7 +6,18 @@ namespace FamilyTree
     {
         static void Main()
         {
-            
+            var db = new SqlDatabase();
+            if (db.CreateDatabase())
+            {
+                db.CreateTable("Family", 
+                    "id int PRIMARY KEY IDENTITY (1,1) NOT NULL, " +
+                    "first_name nvarchar(50) NOT NULL, " +
+                    "last_name nvarchar(50) NOT NULL, " +
+                    "date_of_birth date NULL, " +
+                    "date_of_death date NULL, " +
+                    "mother_id int NULL, " +
+                    "father_id int NULL");
+            }
         }
     }
 }
---
+++ FamilyTree/SqlDatabase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading;
+
+namespace FamilyTree
+{
+    class SqlDatabase
+    {
+        public string ConnectionString { get; set; } = @"Data Source = .\SQLExpress; Integrated Security = true; database = {0}";
+        public string DatabaseName { get; set; }
+
+        public bool CreateDatabase()
+        {
+            try
+            {
+                DatabaseName = "master";
+                ExecuteSql("CREATE DATABASE FamilyTree");
+                DatabaseName = "FamilyTree";
+                return true;
+            }
+            catch
+            {
+                DatabaseName = "FamilyTree";
+                return false;
+            }
+        }
+
+        public void CreateTable(string name, string columns)
+        {
+            ExecuteSql($"CREATE TABLE {name} ({columns})");
+        }
+        
+        private void ExecuteSql(string sql, params (string, string)[] parameters)
+        {
+            var connectionString = string.Format(ConnectionString, DatabaseName);
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.AddWithValue(parameter.Item1, parameter.Item2);
+                    }
+
+                    foreach (SqlParameter parameter in command.Parameters)
+                    {
+                        if (parameter.Value.ToString() == "null")
+                        {
+                            parameter.Value = DBNull.Value;
+                        }
+                    }
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
